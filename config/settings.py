b76b8ba4921ba32b0f1ca9b165a3df881861d8c4
Oscardk <dauren2050@yandex.ru>
2024-07-21 18:02:43 +0500

    add new and fix code v 1

--- config/settings.py
+++ config/settings.py
@@ -68,6 +68,12 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_HEADERS = "*"
 
+CORS_ORIGIN_WHITELIST = [
+
+    "https://marina-backender.org.kg"
+
+]
+
 ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
